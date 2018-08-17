9a20e949f8cd232f863da88be582ab643e7479b9
branch <browningkk@163.com>
2018-08-17 17:06:35 +0800

    漫反射

--- OpenGL0801/fs.fs
+++ OpenGL0801/fs.fs
@@ -1,11 +1,25 @@
 #version 330 core
 out vec4 FragColor;
 
-uniform vec3 objectColor;
+in vec3 Normal;
+in vec3 FragPos;
+
+uniform vec3 lightPos;
 uniform vec3 lightColor;
+uniform vec3 objectColor;
 
 void main()
 {
-	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	FragColor = vec4(objectColor*lightColor, 1.0f);
+	// ambient
+	float ambientStrength = 0.1;
+	vec3 ambient = ambientStrength * lightColor;
+
+	// diffuse 
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(lightPos - FragPos);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * lightColor;
+
+	vec3 result = (ambient + diffuse) * objectColor;
+	FragColor = vec4(result, 1.0);
 }
